a6167c5ee0a67ff10fb65152abcf56cad60f7edc
Anthony Lai <anthonylai89@gmail.com>
2011-02-27 23:08:50 -0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masterater::Application.config.secret_token = '2b3640d05c01191bb827e6a0dcfca2ab0fc68d448454f2dbd14d8817c69920bdc4b3eab95bf8aa5508b75e1947d0c953a3b26afbd484aadf8a8ef94bd3597280'
