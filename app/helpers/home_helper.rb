3b3709f548a4a7ef7cf5cce55436a054a4273c85
Anthony Lai <anthonylai89@gmail.com>
2011-03-02 03:39:50 -0800

    all genders male

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,12 @@ module HomeHelper
   def uid_img(user)
     "http://graph.facebook.com/#{user.uid}/picture?type=large"
   end
+
+  def opposite_sex(user)
+    if user.gender == 'female'
+      'male'
+    else
+      'female'
+    end
+  end
 end
