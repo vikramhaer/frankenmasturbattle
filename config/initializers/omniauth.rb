76c74f93ea9163e6fc2d6491cfc07c4a69e8e4a2
Anthony Lai <anthonylai89@gmail.com>
2011-03-02 03:11:54 -0800

    fixed development/production keys for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
-if Rails.env == "Production"
+if Rails.env == "production"
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, '107180036028128', '185e91e4dad9738c2f68d9ad3c2fac40', {:scope => 'publish_stream,email,friends_education_history,friends_location,friends_work_history', :display => "popup"}
+  end
+elsif Rails.env == "development"
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, '171400646241780', 'a375c57c7719cc4f08d2d704247a8319', {:scope => 'publish_stream,email,friends_education_history,friends_location,friends_work_history', :display => "popup"}
   end
 else
   Rails.application.config.middleware.use OmniAuth::Builder do
