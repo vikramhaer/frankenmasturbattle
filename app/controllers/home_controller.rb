9bc57196ffe12fe82ea4b7299ffaea18975e5412
Anthony Lai <anthonylai89@gmail.com>
2011-03-04 00:09:20 -0800

    added last battle history and dried up bground partial a bit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,7 @@ class HomeController < ApplicationController
   end
   
   def battle
+    session[:last_battle] = session[:battle_uids]
     if params[:choice] == "left" then @dscore = User.update_win_loss_by_uid(session[:battle_uids][0]['uid'], session[:battle_uids][1]['uid']) end
     if params[:choice] == "right" then @dscore = User.update_win_loss_by_uid(session[:battle_uids][1]['uid'], session[:battle_uids][0]['uid']) end
     if @dscore == -1 then raise session[:battle_uids].to_yaml end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,11 +3,27 @@ module HomeHelper
     "http://graph.facebook.com/#{user.uid}/picture?type=large"
   end
 
-  def left_user
-    session[:battle_uids][0]
+  def left_user_name
+    session[:battle_uids][0]["name"]
   end
 
-  def right_user
-    session[:battle_uids][1]
+  def right_user_name
+    session[:battle_uids][1]["name"]
+  end
+
+  def left_user_pic
+    image_tag "http://graph.facebook.com/#{session[:battle_uids][0].uid}/picture?type=large"
+  end
+
+  def right_user_pic
+    image_tag "http://graph.facebook.com/#{session[:battle_uids][1].uid}/picture?type=large"
+  end
+
+  def last_left_pic
+    image_tag "http://graph.facebook.com/#{session[:last_battle][0].uid}/picture?type=small" if session[:last_battle]
+  end
+
+  def last_right_pic
+    image_tag "http://graph.facebook.com/#{session[:last_battle][1].uid}/picture?type=small" if session[:last_battle]
   end
 end
