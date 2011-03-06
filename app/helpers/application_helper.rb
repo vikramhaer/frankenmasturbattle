76841fc730f8be93c667141182cbd544193b7d75
Anthony Lai <anthonylai89@gmail.com>
2011-03-05 19:11:53 -0800

    moved friend-box code into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def user_tile(user)
+    link_to(
+      content_tag(:div, 
+        image_tag("http://graph.facebook.com/#{user.uid}/picture", :class=>"battle") + 
+        content_tag(:p, user.name, :class => "boldit lastround") +
+        content_tag(:p, user.score.to_i, :class => "boldit score"), 
+        :class => "friend-box"), 
+      user, :class => "no-style")
+  end
 
 end
 
