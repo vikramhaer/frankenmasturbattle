8876959c978b065001911c2b6ee97f448eccd153
Anthony Lai <anthonylai89@gmail.com>
2011-03-08 13:59:41 -0800

    added www to root redirect code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Masterater::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.middleware.use "WwwMiddleware"
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
