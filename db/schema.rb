513a8988dcbdaed28d0011d6222fee2b052739f7
Vikram Haer <vikramhaer@gmail.com>
2011-03-08 21:58:31 -0800

    removed irrelevant fonts to speed up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110308061047) do
+ActiveRecord::Schema.define(:version => 20110308063434) do
 
   create_table "betas", :force => true do |t|
     t.string   "email"
