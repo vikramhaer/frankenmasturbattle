aac602a4d6548467e4db728117e0bbbfae2c7bb6
Anthony Lai <anthonylai89@gmail.com>
2011-03-26 19:22:26 -0700

    fixed settings page to not include users_path

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,17 +44,28 @@ class User < ActiveRecord::Base
       user.name = auth["user_info"]["name"]
       user.gender = auth["extra"]["user_hash"]["gender"]
     end
+    Crewait.start_waiting
+    usa = User.crewait(:name => "something", :uid => "blah")
+    Crewait.go!
+    usb = User.create(:name => "somethingagain", :uid => "oncemore")
+    Crewait.start_waiting
+    usc = User.crewait(:name => "omgmore?", :uid => "1234")
+    Crewait.go!
+    raise User.all.to_yaml
+
   end
 
   def login_procedure(auth)
-    if self.login_count == 0
-      self.update_attributes(:settings => 4465285)
-      if self.score < 1000 then self.update_attributes(:score => 1000) end
-    end
-    self.increment_login_count
     self.update_info(auth)
     self.update_groups(auth)
-    self.update_friends(auth)
+
+    if self.login_count == 0
+      self.update_attributes(:settings => 4465285)
+      self.update_friends(auth)
+      if self.score < 1000 then self.update_attributes(:score => 1000) end
+    end
+
+    self.increment_login_count
   end
 
 
@@ -71,7 +82,6 @@ class User < ActiveRecord::Base
     #fq = FbGraph::Query.new("SELECT work_history,education_history,current_location FROM user where uid=#{auth["uid"]}").fetch(auth["credentials"]["token"])
     hash = auth['extra']['user_hash']
     all_groups = self.groups
-    
     location = hash['location']
     if location
       name_formatted = location['name'].each { |word| word.capitalize! }
@@ -90,35 +100,84 @@ class User < ActiveRecord::Base
   end
 
   def update_friends(auth)
+    #a = Time.now
     fbquery = "SELECT uid, name, sex, current_location, education_history FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 =#{auth["uid"]})"
     fbq_friends = FbGraph::Query.new(fbquery).fetch(auth["credentials"]["token"]) #this might take a while...
-    begin_time = Time.now
+    #b = Time.now
     users_from_fbq = User.where(:uid => fbq_friends.collect { |fbq_friend| fbq_friend["uid"].to_s } )
     hashed_users_from_fbq = Hash[ users_from_fbq.collect { |user| [user.uid, user] } ]
 
     friends_not_added = users_from_fbq - self.friends
-    friends_not_created = fbq_friends.reject{ |fbq_friend| hashed_users_from_fbq.include?(fbq_friend["uid"].to_s) }  
+    friends_not_created = fbq_friends.reject{ |fbq_friend| hashed_users_from_fbq.include?(fbq_friend["uid"].to_s) }
     
-    if !friends_not_added.empty? or !friends_not_created.empty?
+
+    #add groups for all the new people
+    groups_not_created = []
+    if !friends_not_created.empty?
+      #get all the groups of the friends not created yet
+      friends_not_created_groups = friends_not_created.collect{ |fq| helper_get_group_names_from_fq(fq) }.flatten!.reject!{|g| g == nil}.uniq!
+      #find all the groups already in the db
+      groups_from_fbq = Group.where(:name => friends_not_created_groups)
+      #hash them for fast lookup later
+      hashed_groups_from_fbq = Hash[ groups_from_fbq.collect { |group| [group.name, group.id] } ]
+      #figure out which were not created yet by comparing the original list with the results from db
+      groups_not_created = friends_not_created_groups.reject {|group_name| hashed_groups_from_fbq.include?(group_name) }
+    end
+
+    #c = Time.now
+
+    #raise ((b-a)*1000).to_s + " " + ((c-b)*1000).to_s #+ friends_not_created_groups.to_yaml
+    if !friends_not_added.empty? or !friends_not_created.empty? or !groups_not_created.empty?
       Crewait.start_waiting
+      groups_not_created.each do |group_name|
+        #create each group
+        group = Group.crewait(:name => group_name)
+        #add each group to the hash to add memberships later on
+        hashed_groups_from_fbq[group_name] = group.id
+      end
       friends_not_added.each do |friend|
         Friendship.crewait(:user_id => self.id, :friend_id => friend.id)
       end
-
+      statlist = []
       friends_not_created.each do |fbq_friend|
+        #create the friend
         friend = User.crewait(:uid => fbq_friend["uid"].to_s, :name => fbq_friend["name"], :gender => fbq_friend["sex"])
+        #add the friendship
         Friendship.crewait(:user_id => self.id, :friend_id => friend.id)
-      end    
+        #find the friend's groups
+        groups_to_join = helper_get_group_names_from_fq(fbq_friend).uniq.reject{|g| g == nil}
+        statlist << (fbq_friend["name"] + "[" + friend.id.to_s + "]:" + groups_to_join.collect{|group_name| group_name + "[" + hashed_groups_from_fbq[group_name].to_s + "]"} * ",")
+        groups_to_join.each do |group_name|
+          #add memberships for each group
+          Membership.crewait(:user_id => friend.id, :group_id => hashed_groups_from_fbq[group_name])
+        end
+      end
+      #raise statlist.to_yaml
       Crewait.go!
     end
-
+    #d = Time.now
     #insert group insertion code here
-    friends_not_created.each do |fbq_friend|         #THIS IS SLOWWWWWW
-      user = User.where(:uid => fbq_friend["uid"].to_s).first
-      user.update_groups_with_fq(fbq_friend)
-    end
+    #friends_not_created.each do |fbq_friend|         #THIS IS SLOWWWWWW
+    #  user = User.where(:uid => fbq_friend["uid"].to_s).first
+    #  user.update_groups_with_fq(fbq_friend)
+    #end
+    #raise "fbquery = #{(b-a)*1000}ms, sorting_and_call = #{(c-b)*1000}ms, insertion= #{(d-c)*1000}ms"
     #raise "update_friends took #{(Time.now - begin_time)*1000} ms"
   end
+
+  def helper_get_group_names_from_fq(fq)
+    rtn = []
+    rtn << fq['current_location']['name'] if fq['current_location']
+
+    if fq['education_history']
+      fq['education_history'].each do |edu|
+        name_formatted = edu['name'].each { |word| word.capitalize! }
+        rtn << name_formatted
+      end
+    end
+    return rtn
+  end
+    
 
   def update_groups_with_fq(fq) #Only has location atm, need to add education!
     if fq['current_location']
